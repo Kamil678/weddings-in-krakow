9f5b5b39958dc807b7854fbc21e636b076929905
Kamil678 <kamilpigulak@interia.pl>
2024-12-11 09:57:55 +0100

    add header

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,96 +1,32 @@
 <template>
   <header class="header">
     <nav class="header__nav">
-      <router-link to="/" class="header__logo">
-        <img
-          src="../../assets/shared/logo.png"
-          alt="Wedding friends logo"
-          class="header__logo__image"
-        />
-      </router-link>
-      <ul
-        :class="{ 'header__nav__list--open': openMenu }"
-        class="header__nav__list"
-      >
-        <li>
-          <button @click="openMenu = false" class="header__nav__close-menu-btn">
-            &times;
-          </button>
-        </li>
-        <li class="header__nav__list__element">
-          <router-link
-            to="/"
-            class="header__nav__list__link"
-            active-class="active"
-            @click="openMenu = false"
-            >{{ $t("navHome") }}</router-link
-          >
-        </li>
-        <li class="header__nav__list__element">
-          <router-link
-            to="/offer"
-            class="header__nav__list__link"
-            active-class="active"
-            @click="openMenu = false"
-            >{{ $t("navOffer") }}</router-link
-          >
-        </li>
-        <li class="header__nav__list__element">
-          <router-link
-            to="/humanist-weddings"
-            class="header__nav__list__link"
-            active-class="active"
-            @click="openMenu = false"
-            >{{ $t("navHumanistWeddings") }}</router-link
-          >
-        </li>
-        <li class="header__nav__list__element">
-          <router-link
-            to="/wedding-day-coordination"
-            class="header__nav__list__link"
-            active-class="active"
-            @click="openMenu = false"
-            >{{ $t("navWeddingDayCoordination") }}</router-link
-          >
-        </li>
-        <li class="header__nav__list__element">
-          <router-link
-            to="/portfolio"
-            class="header__nav__list__link"
-            active-class="active"
-            @click="openMenu = false"
-            >{{ $t("navProtfolio") }}</router-link
-          >
-        </li>
-        <li class="header__nav__list__element">
-          <router-link
-            to="/opinions"
-            class="header__nav__list__link"
-            active-class="active"
-            @click="openMenu = false"
-            >{{ $t("navOpinions") }}</router-link
-          >
-        </li>
-        <li class="header__nav__list__element">
-          <router-link
-            to="/offer-halls"
-            class="header__nav__list__link"
-            active-class="active"
-            @click="openMenu = false"
-            >{{ $t("navOfferHalls") }}</router-link
-          >
-        </li>
-        <li class="header__nav__list__element">
-          <router-link
-            to="/contact"
-            class="header__nav__list__link"
-            active-class="active"
-            @click="openMenu = false"
-            >{{ $t("navContact") }}</router-link
-          >
-        </li>
-      </ul>
       <div class="header__nav__buttons-wrap">
+        <button
+          v-if="!openMenu"
+          @click="openMenu = true"
+          class="header__nav__buttons-wrap__mobile-menu-btn"
+        >
+          <img
+            src="../../assets/shared/icon-hamburger.svg"
+            alt="Hamburger icon"
+            class="hamburegr-image"
+          />
+        </button>
+        <button
+          v-if="openMenu"
+          @click="(openMenu = false), (isOpenChangeLanguageMenu = false)"
+          class="header__nav__close-menu-btn"
+        >
+          &times;
+        </button>
+        <router-link to="/" class="header__logo">
+          <img
+            src="../../assets/shared/logo.png"
+            alt="Wedding friends logo"
+            class="header__logo__image"
+          />
+        </router-link>
         <div ref="dropdownRef" class="dropdown" @click="toggleDropdown">
           <button class="dropdown__button">
             <img
@@ -124,22 +60,121 @@
             </li>
           </ul>
         </div>
-        <button
-          @click="openMenu = true"
-          class="header__nav__buttons-wrap__mobile-menu-btn"
-        >
-          <img
-            src="../../assets/shared/icon-hamburger.svg"
-            alt="Hamburger icon"
-            class="hamburegr-image"
-          />
-        </button>
       </div>
+      <ul
+        :class="{ 'header__nav__list--open': openMenu }"
+        class="header__nav__list"
+      >
+        <li class="header__nav__list__element">
+          <router-link
+            to="/"
+            class="header__nav__list__link"
+            active-class="active"
+            >{{ $t("navHome") }}</router-link
+          >
+        </li>
+        <li class="header__nav__list__element">
+          <router-link
+            to="/offer"
+            class="header__nav__list__link"
+            active-class="active"
+            >{{ $t("navOffer") }}</router-link
+          >
+        </li>
+        <li class="header__nav__list__element">
+          <router-link
+            to="/humanist-weddings"
+            class="header__nav__list__link"
+            active-class="active"
+            >{{ $t("navHumanistWeddings") }}</router-link
+          >
+        </li>
+        <li class="header__nav__list__element">
+          <router-link
+            to="/wedding-day-coordination"
+            class="header__nav__list__link"
+            active-class="active"
+            >{{ $t("navWeddingDayCoordination") }}</router-link
+          >
+        </li>
+        <li class="header__nav__list__element">
+          <router-link
+            to="/portfolio"
+            class="header__nav__list__link"
+            active-class="active"
+            >{{ $t("navProtfolio") }}</router-link
+          >
+        </li>
+        <li class="header__nav__list__element">
+          <router-link
+            to="/opinions"
+            class="header__nav__list__link"
+            active-class="active"
+            >{{ $t("navOpinions") }}</router-link
+          >
+        </li>
+        <li class="header__nav__list__element">
+          <router-link
+            to="/offer-halls"
+            class="header__nav__list__link"
+            active-class="active"
+            >{{ $t("navOfferHalls") }}</router-link
+          >
+        </li>
+        <li class="header__nav__list__element">
+          <router-link
+            to="/contact"
+            class="header__nav__list__link"
+            active-class="active"
+            >{{ $t("navContact") }}</router-link
+          >
+        </li>
+        <div
+          ref="dropdownRef"
+          class="dropdown dropdown--small-screen"
+          @click="toggleDropdown"
+        >
+          <button class="dropdown__button dropdown__button--small-screen">
+            <img
+              :src="currentLanguage.flagImg"
+              height="16"
+              class="dropdown__button__img-flag"
+            />
+            {{ currentLanguage.label }}
+            <span class="dropdown__button__arrow"
+              ><img
+                v-if="isOpenChangeLanguageMenu"
+                src="../../assets/shared/chevron-up-ico-white.svg" />
+              <img
+                v-if="!isOpenChangeLanguageMenu"
+                src="../../assets/shared/chevron-down-ico-white.svg"
+            /></span>
+          </button>
+          <ul
+            v-if="isOpenChangeLanguageMenu"
+            class="dropdown__menu dropdown__menu--small-screen"
+          >
+            <li
+              v-for="lang in languages"
+              :key="lang.code"
+              @click.stop="changeLanguage(lang.code)"
+              class="dropdown__menu__item"
+            >
+              <img
+                :src="lang.flagImg"
+                height="14"
+                class="dropdown__button__img-flag"
+              />
+              {{ lang.label }}
+            </li>
+          </ul>
+        </div>
+      </ul>
     </nav>
   </header>
 </template>
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from "vue";
+import { ref } from "vue";
 import { useI18n } from "vue-i18n";
 
 const openMenu = ref(false);
@@ -191,22 +226,7 @@ const changeLanguage = (languageCode) => {
   isOpenChangeLanguageMenu.value = false;
   locale.value = languageCode;
   localStorage.setItem("language", languageCode);
-  openMenu.value = false;
 };
-
-const handleClickOutside = (event) => {
-  if (dropdownRef.value && !dropdownRef.value.contains(event.target)) {
-    isOpenChangeLanguageMenu.value = false;
-  }
-};
-
-onMounted(() => {
-  document.addEventListener("click", handleClickOutside);
-});
-
-onBeforeUnmount(() => {
-  document.removeEventListener("click", handleClickOutside);
-});
 </script>
 
 <style lang="scss">
@@ -215,56 +235,58 @@ onBeforeUnmount(() => {
   top: 0;
   left: 0;
   width: 100%;
-  padding: 0 3rem;
   z-index: 9999;
   background-color: #fff;
 
   &__nav {
     width: 100%;
     margin: 0 auto;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
 
     &__close-menu-btn {
       background-color: transparent;
-      color: #fff;
+      color: #000;
       border: none;
-      font-size: 30px;
-      text-align: end;
-      position: absolute;
-      right: 20px;
+      font-size: 43.5px;
+      min-width: auto;
+      text-align: start;
+      cursor: pointer;
 
-      @media (min-width: $xxxl-screen) {
-        display: none;
+      @media (min-width: $sm-screen) {
+        min-width: 90px;
       }
     }
 
     &__list {
-      display: none;
-      gap: 1.5rem 2rem;
+      position: relative;
+      top: -555px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 1.5rem;
       list-style: none;
+      background-color: $primary-color;
+      padding: 1rem;
+      transition: top 0.3s ease-out, transform 0.3s ease-out;
+      background-color: rgba(161, 109, 139, 0.6);
+      backdrop-filter: blur(10px);
+      -webkit-backdrop-filter: blur(10px);
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+      color: white;
 
-      &--open {
-        display: flex;
-        background-color: $primary-color;
-        flex-direction: column;
-        position: absolute;
-        top: 0;
-        right: 0;
-        min-width: 300px;
-        align-items: flex-start;
-        z-index: 9999;
-        padding: 30px 20px;
-        height: 100vh;
-
-        .header__nav__list__link {
-          color: #fff;
-        }
+      @media (min-width: $sm-screen) {
+        top: -515px;
       }
 
-      @media (min-width: $xxxl-screen) {
-        display: flex;
+      @media (min-width: $xl-screen) {
+        top: -75px;
+        gap: 1.5rem 3rem;
+        flex-direction: row;
+        transition: top 0.2s ease-out, transform 0.2s ease-out;
+      }
+
+      &--open {
+        top: 0;
       }
 
       &__element {
@@ -276,12 +298,13 @@ onBeforeUnmount(() => {
           height: 3px;
           content: "";
           position: absolute;
-          background-color: $primary-color;
+          background-color: #fff;
           width: 0%;
           bottom: 0;
         }
 
         &:has(.active) {
+          font-weight: 500;
           &::before {
             width: 100%;
           }
@@ -317,7 +340,7 @@ onBeforeUnmount(() => {
       &__link {
         text-transform: uppercase;
         text-decoration: none;
-        color: #000;
+        color: #fff;
         font-size: 1rem;
         line-height: 18px;
         transition: ease 0.3s;
@@ -327,55 +350,15 @@ onBeforeUnmount(() => {
     &__buttons-wrap {
       display: flex;
       align-items: center;
+      justify-content: space-between;
       gap: 20px;
+      padding: 0 1.5rem;
+      position: relative;
+      z-index: 2;
+      background-color: #fff;
 
-      .dropdown {
-        position: relative;
-        display: inline-block;
-        padding: 10px 0;
-
-        &__button {
-          background-color: transparent;
-          border: none;
-          padding: 10px 15px;
-          font-size: 16px;
-          border-radius: 4px;
-          cursor: pointer;
-          display: flex;
-
-          &__img-flag {
-            margin-right: 10px;
-          }
-
-          &__arrow {
-            margin-left: 8px;
-
-            img {
-              vertical-align: middle;
-            }
-          }
-        }
-
-        &__menu {
-          position: absolute;
-          background-color: white;
-          border: 1px solid #ccc;
-          border-radius: 4px;
-          margin-top: 4px;
-          list-style: none;
-          padding: 0;
-          z-index: 1000;
-
-          &__item {
-            padding: 8px 16px;
-            cursor: pointer;
-
-            &:hover {
-              background-color: $primary-color;
-              color: #ffffff;
-            }
-          }
-        }
+      @media (min-width: $md-screen) {
+        padding: 0 3rem;
       }
 
       &__mobile-menu-btn {
@@ -383,14 +366,89 @@ onBeforeUnmount(() => {
         background-color: transparent;
         border: none;
         cursor: pointer;
+        min-width: auto;
+        text-align: left;
 
         .hamburegr-image {
           width: 1.625rem;
           height: 1.625rem;
         }
 
-        @media (min-width: $xxxl-screen) {
+        @media (min-width: $sm-screen) {
+          min-width: 90px;
+        }
+      }
+    }
+
+    .dropdown {
+      position: relative;
+      display: none;
+      padding: 10px 0;
+      min-width: 90px;
+
+      @media (min-width: $sm-screen) {
+        display: inline-block;
+      }
+
+      &--small-screen {
+        display: inline-block;
+        padding: 0;
+
+        @media (min-width: $sm-screen) {
           display: none;
+        }
+      }
+
+      &__button {
+        background-color: transparent;
+        border: none;
+        padding: 10px 0 10px 15px;
+        font-size: 16px;
+        border-radius: 4px;
+        cursor: pointer;
+        display: flex;
+
+        &--small-screen {
+          padding: 0;
+          color: #fff;
+        }
+
+        &__img-flag {
+          margin-right: 10px;
+        }
+
+        &__arrow {
+          margin-left: 8px;
+
+          img {
+            vertical-align: middle;
+          }
+        }
+      }
+
+      &__menu {
+        position: absolute;
+        background-color: white;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        margin-top: 4px;
+        list-style: none;
+        padding: 0;
+        z-index: 1000;
+
+        &--small-screen {
+          min-width: 90px;
+          color: #000;
+        }
+
+        &__item {
+          padding: 8px 16px;
+          cursor: pointer;
+
+          &:hover {
+            background-color: $primary-color;
+            color: #ffffff;
+          }
         }
       }
     }
@@ -405,7 +463,11 @@ onBeforeUnmount(() => {
     font-family: "Caudex", serif;
 
     &__image {
-      width: 250px;
+      width: 200px;
+
+      @media (min-width: $sm-screen) {
+        width: 240px;
+      }
     }
   }
 }
